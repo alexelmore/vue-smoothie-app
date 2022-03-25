b7b61ccb7b6f115686bdc8527c1bfdf59bfdc867
aelmore49 <alexelmore49@gmail.com>
2022-03-25 12:26:03 -0500

    Navbar setup and Index setup

---
+++ smotthie-app/src/components/Index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="index container">
+    <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
+      <div class="card-content">
+        <i class="material-icons delete">delete</i>
+        <h2 class="indigo-text">
+          {{ smoothie.title }}
+          <ul class="ingredients">
+            <li
+              v-for="(ingredient, index) in smoothie.ingredients"
+              :key="index"
+            >
+              <span class="chip">{{ ingredient }}</span>
+            </li>
+          </ul>
+        </h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {
+      smoothies: [
+        {
+          title: "Chocolate",
+          slug: "chocolate-brew",
+          ingredients: ["chocolate", "milk", "coffee"],
+          id: 1,
+        },
+        {
+          title: "Lemmon",
+          slug: "lemmon-brew",
+          ingredients: ["lemons", "ice", "syrup"],
+          id: 2,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.index {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 30px;
+  margin-top: 60px;
+}
+.index h2 {
+  font-size: 1.8em;
+  text-align: center;
+  margin-top: 0;
+}
+.index .ingredients {
+  margin: 30px auto;
+}
+.index .ingredients li {
+  display: inline-block;
+}
+.index .delete {
+  position: absolute;
+  top: 4px;
+  right: 4px;
+  cursor: pointer;
+  color: #aaa;
+  font-size: 1.4em;
+}
+</style>
--- smotthie-app/src/components/Navbar.vue
+++ smotthie-app/src/components/Navbar.vue
@@ -1,11 +1,29 @@
 <template>
-  <nav>Navbar</nav>
+  <div class="navbar">
+    <nav class="nav-extended indigo darken-2">
+      <div class="nav-content">
+        <router-link to="/">
+          <span class="nav-title">Home</span>
+        </router-link>
+        <a href="" class="btn-floating btn-large halfway-fab pink">
+          <router-link to="/">
+            <i class="material-icons">add</i>
+          </router-link>
+        </a>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Navbar",
+  data() {},
 };
 </script>
 
-<style></style>
+<style>
+.navbar nav {
+  padding: 0 20px;
+}
+</style>
