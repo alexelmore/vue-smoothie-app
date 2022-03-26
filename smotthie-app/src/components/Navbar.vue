5ddaae087a2703c1266ad47d0cc4de7e5756c69f
aelmore49 <alexelmore49@gmail.com>
2022-03-26 18:27:00 -0500

    Setup for adding a new smoothie

--- smotthie-app/src/components/Navbar.vue
+++ smotthie-app/src/components/Navbar.vue
@@ -3,10 +3,10 @@
     <nav class="nav-extended indigo darken-2">
       <div class="nav-content">
         <router-link to="/">
-          <span class="nav-title">Home</span>
+          <span class="nav-title">Vue Smoothie Stand</span>
         </router-link>
         <a href="" class="btn-floating btn-large halfway-fab pink">
-          <router-link to="/">
+          <router-link to="/add-smoothie">
             <i class="material-icons">add</i>
           </router-link>
         </a>
--- smotthie-app/src/router/index.js
+++ smotthie-app/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
+import AddSmoothie from '@/components/AddSmoothie'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'index',
       component: Index
+    }, {
+      path: '/add-smoothie',
+      name: 'addSmoothie',
+      component: AddSmoothie
     }
   ]
 })
